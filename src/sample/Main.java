ae407e714c136f41b941e0874dfaf0e6f1e1c20a
nish <nishanth.vincent1@gmail.com>
2019-04-28 23:40:05 -0700

    wip

---
+++ src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        String css = this.getClass().getResource("/sample/css/dark.css").toExternalForm();
+        root.getStylesheets().add(css);
+ //       root.setStyle("-fx-base: rgba(60, 60, 60, 255);");
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
